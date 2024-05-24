f0e5059ec6fdc9c3f8fc2368e7d20c96c1b6a99f
rosendo <42633099+rosendolu@users.noreply.github.com>
2024-05-24 22:19:11 +0800

    fix: command  detect before install

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 set -e
+
+# Change to the directory where the script is located
 cd "$(dirname "$0")"
 BASE_DIR="$HOME/automate-hooks"
 echo "RootDIR: $(pwd)"
 echo "BASE_DIR: $BASE_DIR"
 
+# Check and install webhook and git if not already installed
+if ! command -v webhook >/dev/null; then
+    sudo apt-get update -y
+    sudo apt-get install webhook git -y
+fi
+
+# Clone or update the repository
 if [ -d "$BASE_DIR" ]; then
     echo "Directory \"$BASE_DIR\" exists"
     cd "$BASE_DIR"
@@ -16,19 +25,17 @@ fi
 
 echo "Current directory: $(pwd)"
 
+# Load environment variables from .env.local if it exists
 if [ -f "$BASE_DIR/.env.local" ]; then
     while IFS='=' read -r key value; do
         key=$(echo "$key" | xargs)
         value=$(echo "$value" | xargs)
-        if [ -z "$value" ]; then
-            value=""
-        fi
         export "$key"="$value"
     done <"$BASE_DIR/.env.local"
     echo "Loaded environment variables:"
 fi
 
-# Check Ubuntu system
+# Check if the system is Ubuntu
 if [ -f /etc/os-release ]; then
     . /etc/os-release
     if [ "$ID" != "ubuntu" ]; then
@@ -40,9 +47,7 @@ else
     exit 1
 fi
 
-sudo apt-get update -y
-sudo apt-get install webhook git -y
-
+# Replace variables in hook.yaml
 HOOKS_CONF="$BASE_DIR/hook.yaml"
 VARIABLES=("SECRET" "BASE_DIR")
 for var in "${VARIABLES[@]}"; do
@@ -54,14 +59,27 @@ for var in "${VARIABLES[@]}"; do
     sed -i "s|{{${var}}}|${value}|g" "$HOOKS_CONF"
 done
 
-# Setup node
-chmod +x "$BASE_DIR/hooks/setup-node.sh"
-source "$BASE_DIR/hooks/setup-node.sh"
+# Install Node.js and npm if not already installed
+if ! command -v node >/dev/null; then
+    chmod +x "$BASE_DIR/hooks/setup-node.sh"
+    source "$BASE_DIR/hooks/setup-node.sh"
+    echo "NPM: $(which npm)"
+fi
 
-# Install pm2 and deploy
-which npm
-npm install -g pm2
-pm2 start "$BASE_DIR/startup.sh" --name webhook --watch
-pm2 startup
-pm2 save
+# Install pm2 if not already installed
+if ! command -v pm2 >/dev/null; then
+    npm install -g pm2
+fi
+
+# Start or restart the webhook application with pm2
+if pm2 ls | grep -q "webhook"; then
+    echo "Webhook is already running. Restarting..."
+    pm2 restart webhook
+else
+    echo "Webhook is not running. Starting..."
+    pm2 start "$BASE_DIR/startup.sh" --name webhook --watch
+    pm2 startup
+    pm2 save
+fi
+
 pm2 ls
